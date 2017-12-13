e076f9cd63cd2239eb8288d7278560b33487f082
jbadan <jenna.badanowski@gmail.com>
2017-12-13 07:03:59 -0800

    router setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect
+  } from 'react-router-dom';
 import './App.css';
+import Input from './Input';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+      <Switch>
+        <Route exact path="/" component={Input} />
+      </Switch>
+    </Router>
     );
   }
 }
---
+++ src/Input.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Input extends Component {
+  render() {
+    return (
+      <div>input</div>
+    );
+  }
+}
+
+export default Input;
